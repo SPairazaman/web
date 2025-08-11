db2d0ae1afe2c73757f016ad4f386bd34d87aaa9
Santos Pairazaman <spairazaman@hotmail.com>
2025-08-11 18:36:57 -0500

    Organizado por bimestres

--- js/clase.js
+++ js/clase.js
@@ -1,7 +1,32 @@
   $( function() {
-        $( "#accordion" ).accordion({
+        $("#bimestres" ).accordion({
             collapsible: true,
             heightStyle: "content",
             active:100
         });
+
+        $("#unidadesBI" ).accordion({
+            collapsible: true,
+            heightStyle: "content",
+            active:100
+        });
+
+        $("#unidadesBII" ).accordion({
+            collapsible: true,
+            heightStyle: "content",
+            active:100
+        });
+
+        $("#unidadesBIII" ).accordion({
+            collapsible: true,
+            heightStyle: "content",
+            active:100
+        });
+
+         $("#unidadesBIV" ).accordion({
+            collapsible: true,
+            heightStyle: "content",
+            active:100
+        });
+
     } );
